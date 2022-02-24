8672deec606dc77c50f9f66a97f0c2cc3cbb49cd
Donatella7 <campaniellodonatella@gmail.com>
2022-02-24 14:18:09 +0100

    avevo dato una classe al posto di un id

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,10 @@
 for (let i = 0; i < 101 ; i++) {
     
     let divElement = document.createElement("div");
-    let boxContainer = document.getElementById("my-box-container");
+    const boxContainer = document.getElementById("my-box-container");
     boxContainer.append(divElement);
+    console.log(divElement);
+    console.log(boxContainer);
 }
 
 

95644dfe4040efd4886e7da7620ecdc16fb00b2e
Donatella7 <campaniellodonatella@gmail.com>
2022-02-24 14:29:31 +0100

    bg color box if % 3 or if % 5

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,25 @@
-for (let i = 0; i < 101 ; i++) {
+for (let i = 0; i < 10 ; i++) {
     
     let divElement = document.createElement("div");
     const boxContainer = document.getElementById("my-box-container");
     boxContainer.append(divElement);
-    console.log(divElement);
-    console.log(boxContainer);
+    boxContainer.classList.add("d-flex")
+    divElement.classList.add("my-box");
+    /* div.classList.add("foo", "bar", "baz"); */
+    
+    if( (i % 3) == 0){
+        
+        boxContainer.append(divElement);
+        divElement.classList.add("my-box-green");
+        
+    } else if ((i % 5) == 0) {
+        
+        boxContainer.append(divElement);
+        divElement.classList.add("my-box-red");
+    } else{
+        boxContainer.append(divElement);
+        divElement.classList.remove("my-box");
+    }
 }
 
 

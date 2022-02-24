b88d1452f230ef01db134862af133b6800df1b66
Donatella7 <campaniellodonatella@gmail.com>
2022-02-24 14:34:53 +0100

    innerHtml + text-center class all'elemento

--- js/myscript.js
+++ js/myscript.js
@@ -3,22 +3,26 @@ for (let i = 0; i < 10 ; i++) {
     let divElement = document.createElement("div");
     const boxContainer = document.getElementById("my-box-container");
     boxContainer.append(divElement);
+
     boxContainer.classList.add("d-flex")
     divElement.classList.add("my-box");
-    /* div.classList.add("foo", "bar", "baz"); */
-    
+
     if( (i % 3) == 0){
         
         boxContainer.append(divElement);
-        divElement.classList.add("my-box-green");
+        divElement.classList.add("my-box-green", "text-center");
+        divElement.innerHTML = ("fizz");
         
     } else if ((i % 5) == 0) {
         
         boxContainer.append(divElement);
-        divElement.classList.add("my-box-red");
+        divElement.classList.add("my-box-red", "text-center");
+        divElement.innerHTML = ("buzz");
+
     } else if (( (i % 3) == 0) && ( (i % 5) == 0) ){
         boxContainer.append(divElement);
-        divElement.classList.remove("my-box");
+        divElement.classList.remove("my-box", "text-center");
+        divElement.innerHTML = ("fizzbuzz");
     }
 }
 

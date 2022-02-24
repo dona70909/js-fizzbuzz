26a6d98422ff9f877c475edef17e32cdc5300c69
Donatella7 <campaniellodonatella@gmail.com>
2022-02-24 14:13:32 +0100

    append non funziona

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 101 ; i++) {
+    
+    let divElement = document.createElement("div");
+    let boxContainer = document.getElementById("my-box-container");
+    boxContainer.append(divElement);
+}
+
+
